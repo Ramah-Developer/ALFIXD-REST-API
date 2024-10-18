383b736292a2e04c78051c1770998d0c5415e891
FiiBotz <99767269+Fiisya@users.noreply.github.com>
2024-10-19 00:48:14 +0700

    Update tiktok.js

--- downloader/tiktok.js
+++ downloader/tiktok.js
@@ -1,51 +1,53 @@
 const axios = require('axios');
 
 module.exports = function(app) {
+  async function tiktok2(message) {
+    try {
+      const encodedParams = new URLSearchParams();
+      encodedParams.set('url', message);
+      encodedParams.set('hd', '1');
 
-async function tiktok2(message) {
-  try {
-    const encodedParams = new URLSearchParams();
-    encodedParams.set('url', message);
-    encodedParams.set('hd', '1');
+      const response = await axios({
+        method: 'POST',
+        url: 'https://tikwm.com/api/',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'Cookie': 'current_language=en',
+          'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36'
+        },
+        data: encodedParams
+      });
 
-    const response = await axios({
-      method: 'POST',
-      url: 'https://tikwm.com/api/',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Cookie': 'current_language=en',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36'
-      },
-      data: encodedParams
-    });
+      const videos = response.data.data;
+      return {
+        no_watermark: videos.play,
+        watermark: videos.wmplay,
+        music: videos.music
+      };
 
-    const videos = response.data.data;
-    return {
-      no_watermark: videos.play,
-      watermark: videos.wmplay,
-      music: videos.music
-    };
-
-  } catch (error) {
-    throw new Error(`Failed to fetch video: ${error.message}`);
-  }
-}
-
-app.get('/tiktok', async (req, res) => {
-  try {
-    const url = req.query.url;
-    if (!url) {
-      return res.status(400).json({ error: 'Parameter "url" tidak ditemukan' });
+    } catch (error) {
+      console.error(error);
+      throw new Error(`Failed to fetch video: ${error.message}`);
     }
-
-    const response = await tiktok2(url);
-    res.status(200).json({
-      status: 200,
-      creator: "ALFIXD",
-      data: response
-    });
-
-  } catch (error) {
-    res.status(500).json({ error: error.message });
   }
-});
+
+  app.get('/tiktok', async (req, res) => {
+    try {
+      const url = req.query.url;
+      if (!url || typeof url !== 'string' || !url.startsWith('https://vm.tiktok.com/')) {
+        return res.status(400).json({ error: 'Parameter "url" tidak valid' });
+      }
+
+      const response = await tiktok2(url);
+      res.status(200).json({
+        status: 200,
+        creator: "ALFIXD",
+        data: response
+      });
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Terjadi kesalahan pada server' });
+    }
+  });
+}
